b61435bd506f40fecc01724695ce5e6d3513b9cb
MrDecter <ghoillus@gmail.com>
2023-11-03 01:30:06 +0300

    Adding basic Event handlers

--- app/src/main/java/com/example/desingtextreader/MainActivity.java
+++ app/src/main/java/com/example/desingtextreader/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.desingtextreader;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText name_text, complaint_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name_text = findViewById(R.id.name_text);
+        complaint_text = findViewById(R.id.complaint_text);
+    }
+
+    public void saveDate(View v){
+        String user_name = name_text.getText().toString();
+        String user_complaint = complaint_text.getText().toString();
+
+        try {
+            FileOutputStream file_output = openFileOutput("user_info.txt", MODE_PRIVATE);
+            file_output.write((user_name + " : " + user_complaint).getBytes());
+            file_output.close();
+            name_text.setText("");
+            complaint_text.setText("");
+            Toast.makeText(this, "Сохранено", Toast.LENGTH_LONG).show();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void loadDate(View v){
+        try {
+            FileInputStream file_input = openFileInput("user_info.txt");
+            InputStreamReader reader = new InputStreamReader(file_input);
+            BufferedReader buf_read = new BufferedReader(reader);
+
+            StringBuilder string_buffer = new StringBuilder();
+            String lines="";
+            while ((lines = buf_read.readLine()) != null){
+                string_buffer.append(lines);
+            }
+            Toast.makeText(this,string_buffer, Toast.LENGTH_LONG).show();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
